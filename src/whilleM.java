f8d36d5bff0f2cd00a3acf3c99b2a0874c0c8fe8
anantjha320 <anantjha0000@gmail.com>
2025-10-21 21:25:25 +0530

    imit commit2

---
+++ src/whilleM.java
@@ -0,0 +1,12 @@
+public class whilleM {
+    public static void main(String args[]){
+        int i =12;
+        while(i<11){
+            System.out.println("Anant Jha");
+        }
+
+        do {
+            System.out.println("Anant Jha");
+        } while(i<11);
+    }
+}
