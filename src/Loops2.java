eb95d47326be487ef036be0603ebabfa07c1416a
anantjha320 <anantjha0000@gmail.com>
2025-10-21 20:50:03 +0530

    imit commit2

---
+++ src/Loops2.java
@@ -0,0 +1,9 @@
+public class Loops2 {
+    public static void main(String args[]){
+// Here i is instead of counter [counter==i]
+ // Here [i=i+1 ==>  i++]
+        for(int i = 0 ;i < 11; i++)   {
+            System.out.println(i+ " ");
+        }
+    }
+}
