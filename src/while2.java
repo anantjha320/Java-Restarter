f084a349e6da72912fa424e7a6c2610f2327f528
anantjha320 <anantjha0000@gmail.com>
2025-10-21 21:07:46 +0530

    imit commit2

---
+++ src/while2.java
@@ -0,0 +1,9 @@
+public class while2 {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i < 11) {
+            System.out.println(i+ "");
+            i= i + 1; // (i=i+1)=> i++;
+        }
+    }
+}
