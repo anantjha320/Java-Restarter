0e3e7c40482836a1669027dba3f6fcb6dd7aeef3
anantjha320 <anantjha0000@gmail.com>
2025-10-25 16:22:38 +0530

    imit commit2

---
+++ src/FunctionsD.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class FunctionsD {
+
+
+    public static int calculateProduct(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String args[]) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Product of 2 number is :" + calculateProduct(a, b));
+
+
+    }
+}
