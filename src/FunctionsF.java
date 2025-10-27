2a573f421b8af68d197024001e6ad0cfe2fd7d9c
anantjha320 <anantjha0000@gmail.com>
2025-10-27 21:33:11 +0530

    imit commit2

---
+++ src/FunctionsF.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class FunctionsF{
+
+     public static void printFactorial(int n){
+//loop
+         int Factorial = 1;
+
+         for(int i=n; i>=1; i--){
+             Factorial= Factorial*i;
+
+         }
+
+         System.out.println(Factorial);
+         return;
+     }
+
+    public static void main(String args[]) {
+         Scanner sc= new Scanner(System.in);
+         int n = sc.nextInt();
+         printFactorial(n);
+
+    }
+}
